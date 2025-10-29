6a63de70f333586aab3c23362d7939d95744f68a
Jonathan Pineda <jonathanpineda@MacBook-Pro-de-Jonathan.local>
2025-10-28 23:54:30 -0300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: required for GitHub Pages project site under jonpineda7/life-clock
+  base: '/life-clock/',
 })
